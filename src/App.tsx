b238bbcb4a8e54caca663e982f389cee24e6b1ac
DenysNazarenko <dinis.nazarenko03@gmail.com>
2024-12-25 11:42:13 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500">
+      <h1 className="text-white text-4xl font-bold">Hello, Tailwind + TS!</h1>
+    </div>
+  );
+}
+
+export default App;
